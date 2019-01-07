e63319d730ae28f6967719d216152b8dfb96247b
Dell <qwertxz>
2019-01-07 09:51:42 +0800

    Basic knowledge of python. It concludes number/list/string

---
+++ HelloWorld.py
@@ -0,0 +1,24 @@
+
+#1.编程也需要浪漫，python中字符串*3，表示这个字符串输出三次，只能和字符串拼接，不能加数字
+a='重要的事情说三遍：'
+b="我爱你"
+print(a+b*3)
+
+c='Hello'
+d=" "
+e="World"
+f="!"
+print((c+d+e+f)*3)
+# 可以将字符串赋值，也可以直接拼接
+print("py"+"thon")
+a="py"
+print(a+"thon")
+word="python"
+print(word[0],word[5],word[-1],word[-3])
+print(word[0:3])#从下标为0的地方开始，到下标为3，但不包括3
+print(word[0:2])
+print(word[-1])
+print(word[-0])
+print(len(word))
+
+
